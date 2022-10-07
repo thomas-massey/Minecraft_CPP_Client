1fe0e04d158bcf6740663d1bded5c99f6b8f1146
Thomas Massey <thomassey476@outlook.com>
2022-10-07 20:32:11 +0100

    Add project files.

---
+++ Minecraft_CPP_Client.cpp
@@ -0,0 +1,13 @@
+﻿// Minecraft_CPP_Client.cpp : Defines the entry point for the application.
+//
+
+#include "Minecraft_CPP_Client.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Bye CMake." << endl;
+	system("pause");
+	return 0;
+}
